9fa8e8116e539db76beffd0eb866b077dfd5dc68
adrian <adrian.linares94@gmail.com>
2022-01-25 16:59:22 -0700

    This one was the hardest so far..

---
+++ src/CH2/PhoneNumberBreakdown.java
@@ -0,0 +1,30 @@
+package CH2;
+
+import java.util.Scanner;
+
+public class PhoneNumberBreakdown {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        long phoneNumber;
+        long first3Digits;
+        long middle3Digits;
+        long last4Digits;
+        long middle3DigitsFinal;
+
+        System.out.println("Beep boop this is the phone number formatter, welcome human...");
+
+        System.out.println("Enter your phone number without any special characters... (I mean it): ");
+        phoneNumber = scnr.nextLong();
+
+        first3Digits =  phoneNumber / 10000000;
+        middle3Digits = phoneNumber % 10000000;
+        middle3DigitsFinal = middle3Digits / 10000;
+        last4Digits = phoneNumber % 10000;
+
+        System.out.println("Beep boop, here ya go: ");
+        System.out.print("(" + first3Digits + ") ");
+        System.out.print(middle3DigitsFinal + "-");
+        System.out.println(last4Digits);
+
+    }
+}
